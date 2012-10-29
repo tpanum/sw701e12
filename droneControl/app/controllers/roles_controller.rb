153ef12fefeb42e0f0ec21837e23d20b9543af3a
EsbenPM <EsbenPM@gmail.com>
2012-10-29 14:29:17 +0100

    added role stuff

---
+++ droneControl/app/controllers/roles_controller.rb
@@ -0,0 +1,55 @@
+class RolesController < ApplicationController
+  def index
+    list
+    render('list')
+  end
+
+  def list
+    @roles = Role.order('name ASC')
+  end
+
+  def show
+    @role = Role.find(params[:id]) 
+  end
+
+  def new
+    @role = Role.new
+  end
+
+  def create
+    @role = Role.new(params[:role])
+
+    if @role.save
+      flash[:notice] = "Role created."
+      redirect_to(:action => 'list')
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @role = Role.find(params[:id])
+  end
+
+  def update
+    @role = Role.find(params[:id])
+
+    if @role.update_attributes(params[:role])
+      flash[:notice] = "Role updated."
+      redirect_to(:action => 'edit', :id => @role.id)
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @role = Role.find(params[:id])
+  end
+
+  def destroy
+    Role.find(params[:id]).destroy
+
+    flash[:notice] = "The Role has been destroyed"
+    redirect_to(:action => 'list')
+  end
+end
