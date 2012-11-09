a0ca0070cb09d98212270e79feb3dc9cb648e884
Bjarke Hesthaven Søndergaard <tixusdk@gmail.com>
2012-11-09 11:07:04 +0100

    Worked on Anders and Bjarkes agent

--- AT/bjarke_anders/MarioAI/src/ch/idsia/agents/controllers/BadAssAgent.java
+++ AT/bjarke_anders/MarioAI/src/ch/idsia/agents/controllers/BadAssAgent.java
@@ -36,6 +36,7 @@ public class BadAssAgent extends BasicMarioAIAgent implements Agent {
 		action[Mario.KEY_LEFT] = false;
 		action[Mario.KEY_RIGHT] = true;
 		int current = 0;
+		boolean enemies = false;
 
 		if (history.size() >= repeatMax) {
 			int occurrence = history.get(0);
@@ -54,19 +55,30 @@ public class BadAssAgent extends BasicMarioAIAgent implements Agent {
 
 		if (stuck && stuckCount < 3) {
 			action[Mario.KEY_JUMP] = isMarioAbleToJump || !isMarioOnGround;
-			//action[Mario.KEY_SPEED] = true;
+			action[Mario.KEY_SPEED] = false;
 			System.out.println("Stuck");
 			stuckCount++;
 		}
 		else {
-			if (stuck && stuckCount > 18) {
+			if (stuck && stuckCount > 30) {
 				stuck = false;
 				stuckCount = 0;
 			}
 			else {
 				stuckCount++;
 			}
-			if (EnemyBelow()) {
+			if (CoinAbove()) {
+				action[Mario.KEY_RIGHT] = false;
+				action[Mario.KEY_LEFT] = false;
+				action[Mario.KEY_JUMP] = isMarioAbleToJump || !isMarioOnGround;
+				current = 8;
+			}
+			else if (CoinBelow() && !stuck) {
+				action[Mario.KEY_RIGHT] = false;
+				action[Mario.KEY_LEFT] = false;
+				current = 9;
+			}
+			else if (EnemyBelow() && !stuck) {
 				action[Mario.KEY_RIGHT] = false;
 				action[Mario.KEY_LEFT] = false;
 				if (shoot) {
@@ -75,6 +87,7 @@ public class BadAssAgent extends BasicMarioAIAgent implements Agent {
 					shoot = true;
 				}
 				current = 2;
+				enemies = true;
 			}
 			else if (EnemyBelowLeft() && !stuck) {
 				action[Mario.KEY_RIGHT] = false;
@@ -86,10 +99,31 @@ public class BadAssAgent extends BasicMarioAIAgent implements Agent {
 					shoot = true;
 				}
 				current = 1;
+				enemies = true;
+			}
+			else if (CoinBehind() && !stuck) {
+				action[Mario.KEY_RIGHT] = false;
+				action[Mario.KEY_LEFT] = true;
+				if (ObstructionBehind()) {
+					action[Mario.KEY_JUMP] = isMarioAbleToJump || !isMarioOnGround;
+				}
+				current = 7;
 			}
 			else if (EnemyAboveRight()) {
-				action[Mario.KEY_JUMP] = isMarioAbleToJump || !isMarioOnGround;
+				action[Mario.KEY_RIGHT] = false;
+				action[Mario.KEY_LEFT] = true;
 				current = 3;
+				enemies = true;
+			}
+			else if (EnemyAhead()) {
+				action[Mario.KEY_JUMP] = isMarioAbleToJump || !isMarioOnGround;
+				if (shoot) {
+					shoot = false;
+				} else {
+					shoot = true;
+				}
+				current = 4;
+				enemies = true;
 			}
 			else if (EnemyBelowRight()) {
 				action[Mario.KEY_JUMP] = isMarioAbleToJump || !isMarioOnGround;
@@ -98,17 +132,19 @@ public class BadAssAgent extends BasicMarioAIAgent implements Agent {
 				} else {
 					shoot = true;
 				}
-				current = 4;
+				current = 5;
+				enemies = true;
 			}
 			else if (ObstructionAhead()) {
 				action[Mario.KEY_JUMP] = isMarioAbleToJump || !isMarioOnGround;
-				//action[Mario.KEY_SPEED] = true;
-				current = 5;
+				action[Mario.KEY_SPEED] = false;
+				current = 6;
 			} 
 			else {
 				action[Mario.KEY_JUMP] = false;
 				action[Mario.KEY_SPEED] = false;
 				System.out.println("Default");
+				current = 0;
 			}
 		}
 
@@ -116,8 +152,9 @@ public class BadAssAgent extends BasicMarioAIAgent implements Agent {
 		if (history.size() > repeatMax) {
 			history.remove(repeatMax);
 		}
-
-		action[Mario.KEY_SPEED] = shoot;
+		if (enemies && !stuck) {
+			action[Mario.KEY_SPEED] = shoot;
+		}
 
 		return action;
 	}
@@ -152,31 +189,29 @@ public class BadAssAgent extends BasicMarioAIAgent implements Agent {
 			return false;
 		}
 	}
+	
+	private boolean ObstructionBehind() {
+		if (
+				getReceptiveFieldCellValue(marioEgoRow, marioEgoCol - 1) == -24 ||
+				getReceptiveFieldCellValue(marioEgoRow - 1, marioEgoCol - 1) == -24 ||
+				getReceptiveFieldCellValue(marioEgoRow, marioEgoCol - 1) == -60 ||
+				getReceptiveFieldCellValue(marioEgoRow - 1, marioEgoCol - 1) == -60 ||
+				getReceptiveFieldCellValue(marioEgoRow, marioEgoCol - 1) == -85 ||
+				getReceptiveFieldCellValue(marioEgoRow - 1, marioEgoCol - 1) == -85
+				) {
+			System.out.println("Obstruction Behind");
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	private boolean EnemyAboveRight() {
 		if (getEnemiesCellValue(marioEgoRow - 1, marioEgoCol + 1) != 0 || 
-				getEnemiesCellValue(marioEgoRow - 1, marioEgoCol + 2) != 0 ||
-				getEnemiesCellValue(marioEgoRow - 1, marioEgoCol + 3) != 0 ||
+				
+				getEnemiesCellValue(marioEgoRow - 2, marioEgoCol + 1) != 0 ||
 
-				getEnemiesCellValue(marioEgoRow - 2, marioEgoCol + 1) != 0 || 
-				getEnemiesCellValue(marioEgoRow - 2, marioEgoCol + 2) != 0 || 
-				getEnemiesCellValue(marioEgoRow - 2, marioEgoCol + 3) != 0 ||
-
-				getEnemiesCellValue(marioEgoRow - 3, marioEgoCol + 1) != 0 || 
-				getEnemiesCellValue(marioEgoRow - 3, marioEgoCol + 2) != 0 || 
-				getEnemiesCellValue(marioEgoRow - 3, marioEgoCol + 3) != 0 ||
-
-				getEnemiesCellValue(marioEgoRow - 4, marioEgoCol + 1) != 0 || 
-				getEnemiesCellValue(marioEgoRow - 4, marioEgoCol + 2) != 0 || 
-				getEnemiesCellValue(marioEgoRow - 4, marioEgoCol + 3) != 0 ||
-
-				getEnemiesCellValue(marioEgoRow - 5, marioEgoCol + 1) != 0 || 
-				getEnemiesCellValue(marioEgoRow - 5, marioEgoCol + 2) != 0 || 
-				getEnemiesCellValue(marioEgoRow - 5, marioEgoCol + 3) != 0 ||
-
-				getEnemiesCellValue(marioEgoRow - 6, marioEgoCol + 1) != 0 || 
-				getEnemiesCellValue(marioEgoRow - 6, marioEgoCol + 2) != 0 || 
-				getEnemiesCellValue(marioEgoRow - 6, marioEgoCol + 3) != 0) {
+				getEnemiesCellValue(marioEgoRow - 3, marioEgoCol + 1) != 0) {
 			System.out.println("Above Right");
 			return true;
 		} else {
@@ -207,15 +242,7 @@ public class BadAssAgent extends BasicMarioAIAgent implements Agent {
 
 				getEnemiesCellValue(marioEgoRow + 5, marioEgoCol - 1) != 0 || 
 				getEnemiesCellValue(marioEgoRow + 5, marioEgoCol - 2) != 0 ||
-				getEnemiesCellValue(marioEgoRow + 5, marioEgoCol - 3) != 0 ||
-
-				getEnemiesCellValue(marioEgoRow + 6, marioEgoCol - 1) != 0 || 
-				getEnemiesCellValue(marioEgoRow + 6, marioEgoCol - 2) != 0 || 
-				getEnemiesCellValue(marioEgoRow + 6, marioEgoCol - 3) != 0 ||
-
-				getEnemiesCellValue(marioEgoRow + 7, marioEgoCol - 1) != 0 || 
-				getEnemiesCellValue(marioEgoRow + 7, marioEgoCol - 2) != 0 || 
-				getEnemiesCellValue(marioEgoRow + 7, marioEgoCol - 3) != 0) {
+				getEnemiesCellValue(marioEgoRow + 5, marioEgoCol - 3) != 0) {
 			System.out.println("Below Left");
 			return true;
 		} else {
@@ -229,10 +256,23 @@ public class BadAssAgent extends BasicMarioAIAgent implements Agent {
 				getEnemiesCellValue(marioEgoRow + 2, marioEgoCol) != 0 ||
 				getEnemiesCellValue(marioEgoRow + 3, marioEgoCol) != 0 ||
 				getEnemiesCellValue(marioEgoRow + 4, marioEgoCol) != 0 ||
-				getEnemiesCellValue(marioEgoRow + 5, marioEgoCol) != 0 ||
-				getEnemiesCellValue(marioEgoRow + 6, marioEgoCol) != 0 ||
-				getEnemiesCellValue(marioEgoRow + 7, marioEgoCol) != 0) {
+				getEnemiesCellValue(marioEgoRow + 5, marioEgoCol) != 0) {
 			System.out.println("Below");
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private boolean EnemyAhead() {
+		if (getEnemiesCellValue(marioEgoRow - 1, marioEgoCol + 1) != 0 || 
+				getEnemiesCellValue(marioEgoRow - 1, marioEgoCol + 2) != 0 || 
+				getEnemiesCellValue(marioEgoRow - 1, marioEgoCol + 3) != 0 ||
+				
+				getEnemiesCellValue(marioEgoRow - 2, marioEgoCol + 1) != 0 || 
+				getEnemiesCellValue(marioEgoRow - 2, marioEgoCol + 2) != 0 || 
+				getEnemiesCellValue(marioEgoRow - 2, marioEgoCol + 3) != 0) {
+			System.out.println("Ahead");
 			return true;
 		} else {
 			return false;
@@ -262,20 +302,55 @@ public class BadAssAgent extends BasicMarioAIAgent implements Agent {
 
 				getEnemiesCellValue(marioEgoRow + 5, marioEgoCol + 1) != 0 || 
 				getEnemiesCellValue(marioEgoRow + 5, marioEgoCol + 2) != 0 || 
-				getEnemiesCellValue(marioEgoRow + 5, marioEgoCol + 3) != 0 ||
-
-				getEnemiesCellValue(marioEgoRow + 6, marioEgoCol + 1) != 0 || 
-				getEnemiesCellValue(marioEgoRow + 6, marioEgoCol + 2) != 0 ||
-				getEnemiesCellValue(marioEgoRow + 6, marioEgoCol + 3) != 0 ||
-
-				getEnemiesCellValue(marioEgoRow + 7, marioEgoCol + 1) != 0 || 
-				getEnemiesCellValue(marioEgoRow + 7, marioEgoCol + 2) != 0 || 
-				getEnemiesCellValue(marioEgoRow + 7, marioEgoCol + 3) != 0) {
+				getEnemiesCellValue(marioEgoRow + 5, marioEgoCol + 3) != 0) {
 			System.out.println("Below Right");
 			return true;
 		} else {
 			return false;
 		}
+	}
+	
+	private boolean CoinBehind() {
+		if (getReceptiveFieldCellValue(marioEgoRow, marioEgoCol - 1) == 2 ||
+				getReceptiveFieldCellValue(marioEgoRow, marioEgoCol - 2) == 2 ||
+				getReceptiveFieldCellValue(marioEgoRow, marioEgoCol - 3) == 2 ||
+				getReceptiveFieldCellValue(marioEgoRow, marioEgoCol - 4) == 2 ||
+				
+				getReceptiveFieldCellValue(marioEgoRow - 1, marioEgoCol - 1) == 2 ||
+				getReceptiveFieldCellValue(marioEgoRow - 1, marioEgoCol - 2) == 2 ||
+				getReceptiveFieldCellValue(marioEgoRow - 1, marioEgoCol - 3) == 2 ||
+				getReceptiveFieldCellValue(marioEgoRow - 1, marioEgoCol - 4) == 2 ||
+				
+				getReceptiveFieldCellValue(marioEgoRow + 1, marioEgoCol - 1) == 2 ||
+				getReceptiveFieldCellValue(marioEgoRow + 1, marioEgoCol - 2) == 2 ||
+				getReceptiveFieldCellValue(marioEgoRow + 1, marioEgoCol - 3) == 2 ||
+				getReceptiveFieldCellValue(marioEgoRow + 1, marioEgoCol - 4) == 2) {
+			System.out.println("Coin Behind");
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean CoinBelow() {
+		if (getReceptiveFieldCellValue(marioEgoRow + 1, marioEgoCol) == 2 ||
+				
+				getReceptiveFieldCellValue(marioEgoRow + 2, marioEgoCol) == 2) {
+			System.out.println("Coin Below");
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean CoinAbove() {
+		if (getReceptiveFieldCellValue(marioEgoRow, marioEgoCol) == 2 ||
+				
+				getReceptiveFieldCellValue(marioEgoRow - 1, marioEgoCol) == 2 ||
+				
+				getReceptiveFieldCellValue(marioEgoRow - 2, marioEgoCol) == 2) {
+			System.out.println("Coin Above");
+			return true;
+		}
+		return false;
 	}
 }
 
